a2a60717c01cda0eb3b7c9fd1be299b612de026a
aplassard <andrew.plassard@gmail.com>
2013-04-03 15:55:47 -0400

    Intial Commit

---
+++ svm/svm.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from sklearn import svm, datasets, neighbors
+
+class SVM(object):
+
+    def __init__(self,data):
+        pass
+
+
+
+
+class TrainingData(object):
+
+    def __init__(self,features,labels):
+        self._features = features
+        self._labels   = labels
+
+
